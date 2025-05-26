ac7004ab81abc8f296b418c905456877bffd0f05
p2770152 <yeyegucor@gmail.com>
2025-05-26 02:25:48 +0100

    Worked on Tutorial Level
    Need to add UI instructions, animations

---
+++ Assets/Scripts/Player/Player Controls/Tutorial.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Tutorial : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool resetPosition = false;
+    public bool spawnUI = false;
+    public bool enableInputActions = false;
+
+    [Header("References")]
+    public Transform resetPos;
+    public GameObject uiObject;
+    public List<InputActionReference> inputActionsToEnable; // Assign in Inspector
+    public GameObject playerObject;
+    public GameObject oldTriggers;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (resetPosition && resetPos != null)
+            {
+                CharacterController cc = playerObject.GetComponent<CharacterController>();
+
+                // Disable the CharacterController before moving
+                if (cc != null)
+                    cc.enabled = false;
+
+                // Reset position
+                playerObject.transform.position = resetPos.position;
+                Debug.Log($"Player reset to: {resetPos.position}");
+
+                // Re-enable the CharacterController after moving
+                if (cc != null)
+                    cc.enabled = true;
+            }
+        
+
+        if (enableInputActions)
+            {
+                EnableInputActions();
+            }
+            else
+            {
+                DisableInputActions();
+            }
+            if (spawnUI && uiObject != null)
+            {
+                uiObject.SetActive(true);
+            }
+
+            if (oldTriggers != null)
+            {
+                Destroy(oldTriggers);
+            }
+        }
+    }
+
+    private void EnableInputActions()
+    {
+        foreach (InputActionReference actionRef in inputActionsToEnable)
+        {
+            if (actionRef != null)
+            {
+                actionRef.action.Enable();
+            }
+        }
+    }
+
+    private void DisableInputActions()
+    {
+        foreach (InputActionReference actionRef in inputActionsToEnable)
+        {
+            if (actionRef != null)
+            {
+                actionRef.action.Disable();
+            }
+        }
+    }
+}
